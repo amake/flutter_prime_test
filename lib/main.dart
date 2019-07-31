c0b1486a601dd8a36ef95056af31a3be896af987
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-08-01 08:39:22 +0900

    Dart isolate implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,158 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Prime Test'),
+      ),
+      body: Center(child: DartPrimes2()),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class DartPrimes extends StatelessWidget {
+  final Iterator<int> _iterator = _genPrimes().iterator;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, i) {
+        final next = _iterator.moveNext();
+        assert(next);
+        return Text(_iterator.current.toString());
+      },
+    );
+  }
+}
+
+class DartPrimes2 extends StatefulWidget {
+  @override
+  _DartPrimes2State createState() => _DartPrimes2State();
+}
+
+class _DartPrimes2State extends State<DartPrimes2> {
+  List _items;
+  Isolate _isolate;
+  StreamSubscription _subscription;
+  ScrollController _scroll;
+
+  @override
+  void initState() {
+    _items = [];
+    _scroll = ScrollController();
+    final receivePort = ReceivePort();
+    Isolate.spawn(_bgGenPrimes, receivePort.sendPort).then((i) => _isolate = i);
+    _subscription = receivePort.transform(EveryNth(250)).listen((prime) {
+      setState(() {
+        _items.add(prime);
+        if (_items.length > 10) {
+          _scroll.jumpTo(_scroll.position.maxScrollExtent);
+        }
+      });
     });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    _scroll.dispose();
+    _isolate?.kill(priority: Isolate.immediate);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return ListView.builder(
+      itemCount: _items.length,
+      controller: _scroll,
+      itemBuilder: (context, i) {
+        return Text(_items[i].toString());
+      },
     );
+  }
+}
+
+Stream<int> _primes = Stream<int>.fromIterable(_genPrimes());
+
+void _bgGenPrimes(SendPort sendPort) {
+  for (final prime in _genPrimes()) {
+    sendPort.send(prime);
+  }
+}
+
+Iterable<int> _genPrimes() sync* {
+  for (int i = 2;; i++) {
+    if (_isPrime(i)) {
+      yield i;
+    }
+  }
+}
+
+bool _isPrime(int n) {
+  if (n == 2) {
+    return true;
+  }
+  for (int i = n - 1; i > 1; i--) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+class EveryNth<S> extends StreamTransformerBase<S, S> {
+  EveryNth(this.n);
+
+  final int n;
+  @override
+  Stream<S> bind(Stream<S> stream) =>
+      Stream.eventTransformed(stream, (sink) => EveryNthSink<S>(n, sink));
+}
+
+class EveryNthSink<S> extends EventSink<S> {
+  EveryNthSink(this.n, this.outSink);
+
+  final int n;
+  final EventSink<S> outSink;
+  int i = 0;
+
+  @override
+  void add(S event) {
+    if (++i % n == 0) {
+      outSink.add(event);
+    }
+  }
+
+  @override
+  void addError(Object error, [StackTrace stackTrace]) {
+    outSink.addError(error, stackTrace);
+  }
+
+  @override
+  void close() {
+    outSink.close();
   }
 }
