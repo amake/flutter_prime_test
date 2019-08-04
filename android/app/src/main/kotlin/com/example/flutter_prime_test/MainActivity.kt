84a7dfc74e736c3c9202b4a7af9d3907ac17d9fe
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-08-04 23:55:40 +0900

    Implement Android platform

--- android/app/src/main/kotlin/com/example/flutter_prime_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_prime_test/MainActivity.kt
@@ -3,11 +3,53 @@ package com.example.flutter_prime_test
 import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), CoroutineScope {
+
+  override val coroutineContext: CoroutineContext = Dispatchers.Main + SupervisorJob()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    val channel = MethodChannel(flutterView, "example.com/platform")
+
+    val primegen = PrimeGenerator { prime ->
+      launch(Dispatchers.Main) {
+        channel.invokeMethod("addPrime", prime)
+      }
+    }
+
+    channel.setMethodCallHandler { call, result ->
+      when(call.method) {
+        "start" -> {
+          launch { primegen.start() }
+          result.success(true)
+        }
+        "cancel" -> {
+          primegen.reset()
+          result.success(true)
+        }
+        "pause" -> {
+          primegen.stop()
+          result.success(true)
+        }
+        "resume" -> {
+          launch { primegen.start() }
+          result.success(true)
+        }
+        else -> result.error("Method unsupported: ${call.method}", null, null)
+      }
+    }
+
     GeneratedPluginRegistrant.registerWith(this)
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    coroutineContext[Job]!!.cancel()
   }
 }
---
+++ android/app/src/main/kotlin/com/example/flutter_prime_test/Primes.kt
@@ -0,0 +1,50 @@
+package com.example.flutter_prime_test
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+fun isPrime(n: Int): Boolean {
+    if (n == 2) {
+        return true;
+    }
+    for (i in (n - 1) downTo 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+fun primes() = generateSequence(2) { it + 1 }.filter { isPrime(it) }
+
+const val reportingInterval = 250
+
+class PrimeGenerator(val consumer: (Int) -> Unit) {
+
+    var stopped = false
+    var primes = primes()
+    var count = 0
+
+    suspend fun start() = withContext(Dispatchers.Default) {
+        stopped = false
+        for (prime in primes) {
+            if (stopped) {
+                break
+            }
+            count++
+            if (count % reportingInterval == 0) {
+                consumer(prime)
+            }
+        }
+    }
+
+    fun stop() {
+        stopped = true
+    }
+
+    fun reset() {
+        stop()
+        primes = primes()
+        count = 0
+    }
+}
